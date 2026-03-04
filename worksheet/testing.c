6d25ccd582d81a4d42cace80e9d030999715d877
Abdulaziz-mf <zwds0437@leeds.ac.uk>
2026-03-04 16:10:15 +0000

    w

--- worksheet/testing.c
+++ worksheet/testing.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void reverse_array(int *arr, int n) {
-  for (int i =0; i <n; i++){
-    arr[i] = arr[n-i-1];
+
+void reverse_array(int n, int *array){
+  int temp;
+  for (int i = 0; i<n/2; i++){
+    temp = array[i];
+    array[i] = array[n-i-1];
+    array[n-i-1] = temp;
   }
 }
 int main(void){
-    int array[] = {1,2,3,4,5,6};
-    
+  int *array;
+  int n = 6;
+  array = calloc(n,sizeof(int));
+  for(int i = 0; i <n; i++){
+    array[i] = i + 2;
+  }
+  reverse_array(n,array);
+  
+  for(int i = 0; i <n; i++){
+    printf("%d ",array[i]);
+  }
+  printf("\n");
+
 }
--- worksheet/worksheet.c
+++ worksheet/worksheet.c
@@ -165,12 +165,12 @@ int find_max(int *arr, int n, int *index) {
   int max = arr[0];
   for(int i = 0; i<n; i++){
     if(arr[i] > max){
+      *index = i;
       max = arr[i];
     }
   }
-  *index = &max;
 
-  return index;
+  return max;
 }
 
 /* ============================================================
